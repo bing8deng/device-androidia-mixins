384ca556762e2ec658d835b53627bbec10f000cf
Deng, Bing <bing.deng@intel.com>
2025-08-20 21:54:13 +0800

    Update for Houdini_16.0.0.39632.
    Update for Houdini_16.0.0.39632.
    
    Tracked-On: OAM-133810
    Signed-off-by: Deng, Bing <bing.deng@intel.com>

--- groups/houdini/true/product.mk
+++ groups/houdini/true/product.mk
@@ -1,19 +1,6 @@
 # Houdini support
-TARGET_SUPPORTS_64_BIT_APPS := true
+PRODUCT_PACKAGES += libhoudini houdini64
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.dalvik.vm.native.bridge=libhoudini.so \
+    ro.dalvik.vm.isa.arm64=x86_64 \
 
-PRODUCT_PACKAGES += libhoudini houdini
-PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm=x86 ro.vendor.enable.native.bridge.exec=1
-
-ENABLE_NATIVEBRIDGE_64BIT := false
-ifeq ($(BOARD_USE_64BIT_USERSPACE),true)
-  ENABLE_NATIVEBRIDGE_64BIT = true
-else
-  ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-    ENABLE_NATIVEBRIDGE_64BIT = true
-  endif
-endif
-ifeq ($(ENABLE_NATIVEBRIDGE_64BIT),true)
-  PRODUCT_PACKAGES += houdini64
-  PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm64=x86_64 ro.vendor.enable.native.bridge.exec64=1
-endif
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.dalvik.vm.native.bridge=libhoudini.so
